3da92dfd3e0ca1bd9e576e26ff860abaff859cfb
softintercom.shit <softintercom.shit@gmail.com>
2018-06-01 15:44:11 +0300

    first commit

---
+++ Assets/Kong Hero/Script/ServiceCalling.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Runtime.InteropServices;
+
+public class ServiceCalling : MonoBehaviour {
+
+	public void FaceBookInvite(){
+//		ServiceManager.Instance.InviteFriend ();
+		inviteFriendOnFB();
+	}
+
+	[DllImport ("__Internal")]
+	private static extern void inviteFriendOnFB();
+
+	private void friendInvited(){
+		PlayerPrefs.SetInt (GlobalValue.Coins, GlobalValue.SavedCoins+50);
+	}
+
+//	public void RemoveAds(){
+//		ServiceManager.Instance.RemoveAds ();
+//	}
+
+	public void WatchVideoAds(int coins){
+		showRewardedVideo (coins);
+//		ServiceManager.Instance.ShowRewardAds ();
+	}
+
+	[DllImport ("__Internal")]
+	private static extern void showRewardedVideo(int coinsNumber);
+
+	private void rewardedVideoWatched(string result){
+		int coinsNumber = int.Parse(result);
+		PlayerPrefs.SetInt (GlobalValue.Coins, GlobalValue.SavedCoins+coinsNumber);
+	}
+
+//	public void BuyItem1(){
+//		ServiceManager.Instance.BuyItem1 ();
+//	}
+//
+//	public void BuyItem2(){
+//		ServiceManager.Instance.BuyItem2 ();
+//	}
+}
